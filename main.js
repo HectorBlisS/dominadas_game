a87b5f790fe41d6d392e92534e563510eaf4640d
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2018-10-23 17:01:50 -0500

    facebook

--- main.js
+++ main.js
@@ -28,8 +28,10 @@ class playGame extends Phaser.Scene{
         ball.setSize(1,1)
         ball.setInteractive();
         //score
-        if(best !== 0 ) this.add.text(6, 6, 'Best: ' + best , { fontFamily: "Avenir",fontSize: '32px', fill: '#000' });
+        //if(best !== 0 ) this.add.text(6, 6, 'Best: ' + best , { fontFamily: "Avenir",fontSize: '32px', fill: '#000' });
         score = this.add.text(36, 36, 'Dominadas: ' + points , { fontFamily: "Avenir",fontSize: '32px', fill: '#000' });
+
+        getScore(this)
 
         ball.on('pointerdown', (pointer) =>{
             console.log("clicastes")
@@ -55,9 +57,26 @@ class playGame extends Phaser.Scene{
         if(ball.y > config.height + 100) {
             //ball.y = -240
             this.isMoving = false
-            if(points > best) best = points
+            if(points > best) {
+                best = points
+                //salvamos el score
+                FBInstant
+                    .getLeaderboardAsync('Dominadas board.' + FBInstant.context.getID())
+                    .then(leaderboard => {
+                        console.log(leaderboard.getName());
+                        return leaderboard.setScoreAsync(best, '{player: "BlisS"}');
+                    })
+                    .then(() => {
+                        console.log('Score saved')
+                        postBoard()
+                        //to let the score update
+                        this.scene.restart();
+                    })
+                    .catch(error => console.error(error));
+
+            }
             points = 0
-            this.scene.restart();
+            
         }
         
     }
@@ -81,12 +100,71 @@ function start(){
     game = new Phaser.Game(config)
 }
 
-const login = document.getElementById('login')
-const title = document.getElementById('title')
-const name = document.getElementById('name')
-document.getElementById('inicio').addEventListener('click', function(){
-    login.style = "display: none"
-    title.innerHTML = name.value
-    start()
-})
+function resize(){
+     const w = window.innerWidth;
+     const h = window.innerHeight;
+     const wr = w/h
+     const r = config.width / config.height
+     console.log(r)
+     console.log(wr)
+     const canvas = document.querySelector('canvas')
+     if(wr < r){
+        canvas.style.width = w + "px"
+        canvas.style.height = (h / r) + "px"
+     }else{
+         canvas.style.width = (h / r ) + "px"
+         canvas.style.height = h + "px"
+     }
+     
+}
 
+// const login = document.getElementById('login')
+// const title = document.getElementById('title')
+// const name = document.getElementById('name')
+// document.getElementById('inicio').addEventListener('click', function(){
+//     login.style = "display: none"
+//     title.innerHTML = name.value
+//     start()
+// })
+
+FBInstant.initializeAsync()
+  .then(function() {  
+        start()      
+    FBInstant.startGameAsync()
+    .then(function() {
+        resize()
+    })
+  });
+
+// window.onload = function(){
+//     start()
+//     resize()
+// }
+
+function postBoard(){
+    FBInstant.updateAsync({
+        action: 'LEADERBOARD',
+        name: 'Dominadas board.' + FBInstant.context.getID()
+      })
+        .then(() => console.log('Update Posted'))
+        .catch(error => console.error(error));
+}
+
+function getScore(self){
+    FBInstant
+    .getLeaderboardAsync('Dominadas board.' + FBInstant.context.getID())
+    .then(leaderboard => leaderboard.getEntriesAsync(10, 0))
+    .then(entries => {
+      for (var i = 0; i < entries.length; i++) {
+          const rank =  entries[i].getRank() 
+          const name = entries[i].getPlayer().getName()
+          const score = entries[i].getScore()
+        self.add.text(6, 6, `${rank}. ${name} : ${score} ` , { fontFamily: "Avenir",fontSize: '32px', fill: '#000' });
+        console.log(
+          entries[i].getRank() + '. ' +
+          entries[i].getPlayer().getName() + ': ' +
+          entries[i].getScore()
+        );
+      }
+    }).catch(error => console.error(error));
+}
